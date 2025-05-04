505929583f69aee34d7e03c61ff5e663cb160484
NTLnee <linh43807@gmail.com>
2025-05-05 00:49:28 +0700

    Add files via upload

---
+++ nhom-5-tkw/javascript.js
@@ -0,0 +1,11 @@
+// tailwind-config.js
+tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['"Segoe UI"', 'Roboto', 'sans-serif'],
+        }
+      }
+    }
+  };
+  
